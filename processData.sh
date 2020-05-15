abec61d21845178d821f23b9d869ca3cce59806c
Matt Nyerges <mnyerges@gmail.com>
2020-05-15 15:46:40 -0400

    add moving average to data and graph, improve filtering

--- processData.sh
+++ processData.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [[ " $@ " =~ " --help " ]]; then
-	echo "Usage: processData [state list]"
-	echo "Where [state list] is a list of strings representing the states you wish to process counties for."
+	echo "Usage: processData [state/county list]"
+	echo "Where [state/county list] is a list of strings representing the states you wish to process counties for, and the counties in that state."
+	echo "By default, only the US and states will be processed/graphed. If you want to process Marion county in Indiana, run:"
+	echo "./processData.sh Indiana Marion"
 	exit
 fi
 
@@ -18,6 +20,13 @@ awk -F "," '
 		NR==2{pcase=$2; pdeath=$3; print $1","$2","$3",0,0"}
 		NR>2{print $1","$2","$3","$2-pcase","$3-pdeath; pcase=$2; pdeath=$3}
 ' ../covid-19-data/us.csv > country/us.csv
+#add moving average for cases and deaths
+awk -F "," '
+	{c[NR]=$4; csum+=$4; d[NR]=$5; dsum+=$5}
+	NR==1{print $1","$2","$3","$4","$5",newcases5daymovingavg,newdeaths5daymovingavg"}
+	NR>1&&NR<=5{print $1","$2","$3","$4","$5",0,0"}
+	NR>5{csum-=c[NR-5]; dsum-=d[NR-5]; print $1","$2","$3","$4","$5","csum/5","dsum/5}
+' country/us.csv > country/tmp.csv && mv country/tmp.csv country/us.csv
 #plot the us
 gnuplot -e "titel='US'; filename='country/us.csv'; outpt='country/us.png" graphCountry
 
@@ -33,6 +42,13 @@ cat ../covid-19-data/us-states.csv | awk -F "," 'NR>1{print $2}' | sort -u | whi
 		NR==2{pcase=$4; pdeath=$5; print $1","$2","$3","$4","$5",0,0"}
 		NR>2{print $1","$2","$3","$4","$5","$4-pcase","$5-pdeath; pcase=$4; pdeath=$5}
 	' "states/${state}.csv" > states/tmp.csv && mv states/tmp.csv "states/${state}.csv"
+	#add moving average for cases and deaths
+	awk -F "," '
+		{c[NR]=$6; csum+=$6; d[NR]=$7; dsum+=$7}
+		NR==1{print $1","$2","$3","$4","$5","$6","$7",newcases5daymovingavg,newdeaths5daymovingavg"}
+		NR>1&&NR<=5{print $1","$2","$3","$4","$5","$6","$7",0,0"}
+		NR>5{csum-=c[NR-5]; dsum-=d[NR-5]; print $1","$2","$3","$4","$5","$6","$7","csum/5","dsum/5}
+	' "states/${state}.csv" > states/tmp.csv && mv states/tmp.csv "states/${state}.csv"
 	#plot the state
 	gnuplot -e "titel='${state}'; filename='states/${state}.csv'; outpt='states/${state}.png" graphstate
 	
@@ -46,6 +62,15 @@ cat ../covid-19-data/us-states.csv | awk -F "," 'NR>1{print $2}' | sort -u | whi
 	cat ../covid-19-data/us-counties.csv | grep $state | awk -F "," '{print $4}' | sort -u  | awk 'NF' | while read -r mip; do
 		#find county name for mip
 		county=$(cat ../covid-19-data/us-counties.csv | grep $mip | head -n 1 | awk -F "," '{print $2}')
+		if [[ " $@ " =~ " $county " ]]; then
+			echo "Processing county $county"
+		else
+			continue
+		fi
+		if [[ "$state" == "$county" ]]; then
+			#some states have other states as counties!
+			continue
+		fi
 		echo -n "$state-$mip-${county}..."
 		# create a county specific data file
 		cat ../covid-19-data/us-counties.csv | grep date > "counties/${state}-${mip}-${county}.csv"
@@ -55,6 +80,13 @@ cat ../covid-19-data/us-states.csv | awk -F "," 'NR>1{print $2}' | sort -u | whi
 			NR==1{print $1","$2","$3","$4","$5","$6",newcases,newdeaths"}
 			NR==2{pcase=$5; pdeath=$6; print $1","$2","$3","$4","$5","$6",0,0"}
 			NR>2{print $1","$2","$3","$4","$5","$6","$5-pcase","$6-pdeath; pcase=$5; pdeath=$6}
+		' "counties/${state}-${mip}-${county}.csv" > counties/tmp.csv && mv counties/tmp.csv "counties/${state}-${mip}-${county}.csv"
+		#add moving average for cases and deaths
+		awk -F "," '
+			{c[NR]=$7; csum+=$7; d[NR]=$8; dsum+=$8}
+			NR==1{print $1","$2","$3","$4","$5","$6","$7","$8",newcases5daymovingavg,newdeaths5daymovingavg"}
+			NR>1&&NR<=5{print $1","$2","$3","$4","$5","$6","$7","$8",0,0"}
+			NR>5{csum-=c[NR-5]; dsum-=d[NR-5]; print $1","$2","$3","$4","$5","$6","$7","$8","csum/5","dsum/5}
 		' "counties/${state}-${mip}-${county}.csv" > counties/tmp.csv && mv counties/tmp.csv "counties/${state}-${mip}-${county}.csv"
 		#wait for the copy to finish, weird bug
 		sleep .1
